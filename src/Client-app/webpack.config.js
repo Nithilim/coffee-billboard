affb24d4a53293dca1eddabffb7863bc21b440b6
Paulius Kandratavičius <paulius.k@tesonet.com>
2019-11-11 16:22:01 +0200

    add webpack config

---
+++ src/Client-app/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './models/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundlefile.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+}
