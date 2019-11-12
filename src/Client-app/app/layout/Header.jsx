aa174c482c26d97ec7e97c8e07a9f44852243ba6
Paulius Kandratavičius <paulius.k@tesonet.com>
2019-11-12 15:39:36 +0200

    fix structure, add Header

---
+++ src/Client-app/app/layout/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <h1 style={{ position: "absolute" }}>Coffee Billboard</h1>
+      <img src="/public/assets/coffee.jpg" alt="coffee-img" />
+    </div>
+  );
+};
+
+export default Header;
--- src/Client-app/models/index.js
+++ src/Client-app/models/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "../app/layout/app";
+import App from "../app/layout/App";
 
 ReactDOM.render(<App />, document.getElementById("App"));
