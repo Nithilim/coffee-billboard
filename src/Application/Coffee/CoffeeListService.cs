9b86a842248d845392a4534468d74a59ed30ca66
Paulius Kandr <paulius.kandra@gmail.com>
2019-11-16 23:49:51 +0200

    add service methods

--- src/Application/Coffee/CoffeeListService.cs
+++ src/Application/Coffee/CoffeeListService.cs
@@ -3,6 +3,7 @@ using CoffeeModel = Domain.Models.Coffee;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace Application.Coffee
 {
@@ -13,6 +14,36 @@ namespace Application.Coffee
         public CoffeeListService(InMemoryRepository<CoffeeModel> repository)
         {
             _repository = repository;
+        }
+
+        public IQueryable<CoffeeModel> GetAllCoffeeItems()
+        {
+            var items = _repository.Collection();
+            if (items.Any())
+                throw new Exception($"No Items were found");
+
+            return items;
+        }
+
+        public CoffeeModel GetCoffeeItem(Guid id)
+        {
+            var item = _repository.Find(id);
+            if (item == null)
+                throw new Exception($"Item with id ${id} was not found");
+
+            return item;
+        }
+
+        public void RemoveCoffeeItem(Guid id)
+        {
+            var item = _repository.Find(id);
+            if (item != null)
+                _repository.Remove(id);
+        }
+
+        public IQueryable<CoffeeModel> GenerateData()
+        {
+            return new List<CoffeeModel>().AsQueryable();
         }
     }
 }
