877d26ee0cb8356a2b0be3cba1bc8dfc62701697
Paulius Kandr <paulius.kandra@gmail.com>
2019-11-17 21:09:01 +0200

    cleanup, add axios agent

--- src/Client-app/app/layout/App.jsx
+++ src/Client-app/app/layout/App.jsx
@@ -1,43 +1,70 @@
-import React, { useState, Fragment } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Header from "./Header";
 import CoffeeDashboard from "../../features/coffee/dashboard/CoffeeDashboard";
+import agent from "../../api/agent";
+import CoffeeDetails from "../../features/coffee/details/CoffeeDetails";
 
 const App = () => {
-  const [coffeeList, setCoffeeList] = useState([
-    { id: 1, name: "test1", price: 20, description: "adadadada" },
-    { id: 2, name: "test2", price: 22, description: "adadadada" },
-    { id: 3, name: "test3", price: 23, description: "adadadada" },
-    { id: 4, name: "test4", price: 24, description: "adadadada" },
-    { id: 5, name: "test5", price: 25, description: "adadadada" }
-  ]);
-
+  const [coffeeList, setCoffeeList] = useState([]);
   const [coffeeFormOpen, setCoffeeFormOpen] = useState(false);
   const [coffeeDetailsOpen, setCoffeeDetailsOpen] = useState(false);
   const [selectedCoffeeItem, setSelectedCoffeeItem] = useState(null);
 
   const handleCoffeeFormSubmit = addedItem => {
-    setCoffeeList([...coffeeList, addedItem]);
-  };
-  const handleCoffeeItemRemove = removedItem => {
-    setCoffeeList([...coffeeList.filter(i => i.id !== removedItem.id)]);
+    agent.Coffee.create(addedItem).then(() => {
+      setCoffeeList([...coffeeList, addedItem]);
+    });
   };
 
+  const handleCoffeeItemRemove = removedItem => {
+    agent.Coffee.delete(removedItem.id).then(() => {
+      setCoffeeList([...coffeeList.filter(i => i.id !== removedItem.id)]);
+    })
+  };
+
+  const handleRandomSeedGeneration = () => {
+    agent.Coffee.generateRandom().then(response => {
+      setCoffeeList([coffeeList, response.data]);
+    });
+  };
+
+  useEffect(() => {
+    agent.Coffee.getAll().then(response => {
+      setCoffeeList(response.data);
+    });
+  }, []);
+
   return (
-    <Fragment>
+    <Router>
+      <button className="dark-btn" onClick={() => handleRandomSeedGeneration()}/>
       <Header />
-      <CoffeeDashboard
-        coffeeList={coffeeList}
-        setDetailsOpen={setCoffeeDetailsOpen}
-        detailsOpen={coffeeDetailsOpen}
-        formOpen={coffeeFormOpen}
-        setFormOpen={setCoffeeFormOpen}
-        handleFormSubmit={handleCoffeeFormSubmit}
-        handleRemove={handleCoffeeItemRemove}
-        selectedItem={selectedCoffeeItem}
-        setSelectedItem={setSelectedCoffeeItem}
-      />
-    </Fragment>
+      <Switch>
+        <Route path="/" exact>
+          <CoffeeDashboard
+          coffeeList={coffeeList}
+          setDetailsOpen={setCoffeeDetailsOpen}
+          detailsOpen={coffeeDetailsOpen}
+          formOpen={coffeeFormOpen}
+          setFormOpen={setCoffeeFormOpen}
+          handleFormSubmit={handleCoffeeFormSubmit}
+          handleRemove={handleCoffeeItemRemove}
+          selectedItem={selectedCoffeeItem}
+          setSelectedItem={setSelectedCoffeeItem}
+          />
+        </Route>
+        <Route path="/product=:id">
+          {selectedCoffeeItem &&
+            <CoffeeDetails
+            selectedItem={selectedCoffeeItem}
+            setSelectedItem={setSelectedItem}
+            setDetailsOpen={setDetailsOpen}
+            handleRemove={handleRemove}
+          />
+          }
+        </Route>
+      </Switch>
+    </Router> 
   );
 };
 
--- src/Client-app/features/coffee/dashboard/CoffeeDashboard.jsx
+++ src/Client-app/features/coffee/dashboard/CoffeeDashboard.jsx
@@ -1,63 +1,26 @@
-import React from "react";
-import {BrowserRouter as Router, Route, Link} from "react-router-dom";
+import React, {Fragment} from "react";
 import PropTypes from "prop-types";
 import CoffeeItem from "./CoffeeItem";
-import CoffeeDetails from "../details/CoffeeDetails";
-import CoffeeForm from "../form/CoffeeForm";
 
 const CoffeeDashboard = ({
   coffeeList,
-  setDetailsOpen,
-  detailsOpen,
-  formOpen,
-  setFormOpen,
-  handleFormSubmit,
-  handleRemove,
-  selectedItem,
   setSelectedItem
 }) => {
     return (
-      <Router>
-        <Route path="/product=:id">
-        <div>
-          <CoffeeDetails
-            selectedItem={selectedItem}
-            setSelectedItem={setSelectedItem}
-            setDetailsOpen={setDetailsOpen}
-            handleRemove={handleRemove}
-          />
-      </div>
-        </Route>
-        <Route path="/" exact>
-        <div className="card-container">
-        {coffeeList.map(coffee => (
-          <Link to={`/product=${coffee.id}`}>
-            <CoffeeItem key={coffee.id} coffee={coffee} setSelectedItem={setSelectedItem} setDetailsOpen={setDetailsOpen}/>
-          </Link>
-        ))}
-      </div>
-        </Route>
-      </Router>
-    
+        <Fragment>
+        {coffeeList && 
+          <div className="card-container">
+            {coffeeList.map(coffee => (
+              <CoffeeItem key={coffee.id} coffee={coffee} setSelectedItem={setSelectedItem}/>
+              ))}
+          </div>
+        }
+      </Fragment>      
   );
 };
 
-// {formOpen && (
-//   <CoffeeForm
-//     handleFormSubmit={handleFormSubmit}
-//     setFormOpen={setFormOpen}
-//   />
-// )}
-
 CoffeeDashboard.propTypes = {
-  coffeeList: PropTypes.array.isRequired,
-  setDetailsOpen: PropTypes.func,
-  detailsOpen: PropTypes.bool.isRequired,
-  formOpen: PropTypes.bool.isRequired,
-  setFormOpen: PropTypes.func,
-  handleFormSubmit: PropTypes.func.isRequired,
-  handleRemove: PropTypes.func,
-  selectedItem: PropTypes.object,
+  coffeeList: PropTypes.array,
   setSelectedItem: PropTypes.func
 };
 
